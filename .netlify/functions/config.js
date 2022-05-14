9cb5e81a84ece6aad3b6410e4310370e16a22c11
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-05-13 21:40:53 -0500

    edit config

--- .netlify/functions/config.js
+++ .netlify/functions/config.js
@@ -1,13 +1,13 @@
 module.exports = {
     aws_table_name: 'Listing-Purchasers',
     aws_local_config: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
         region: 'us-east-1',
     },
     aws_remote_config: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
         region: 'us-east-1',
     }
 };
