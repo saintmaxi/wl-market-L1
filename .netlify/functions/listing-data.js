680754a2aa37dc78c681d7016991695e5a5f89ba
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-05-14 22:08:27 -0500

    fix cors error

--- .netlify/functions/listing-data.js
+++ .netlify/functions/listing-data.js
@@ -28,7 +28,8 @@ exports.handler = async function(event, context) {
 
          return {
             statusCode: 200,
-            headers: { "content-type": "application/json" },
+            headers: { "content-type": "application/json",
+                        "Access-Control-Allow-Origin": "*"},
             body: JSON.stringify(data)
           }
      
